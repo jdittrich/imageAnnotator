c765c6d8dde6a2c351c4ec76ad77728610a9e34c
dittrich, jan <d_jan@ymail.com>
2015-08-16 21:21:38 +0200

    "imageMainView" now "MainView", since something else could be shown in the main display area too

--- views/AppView.js
+++ views/AppView.js
@@ -7,7 +7,7 @@ var app = app || {};
 app.AppView = Backbone.View.extend({
 //dunno exactly the difference here between Backbone.Layout.extend and Backbone.Layout. The latter needs a direct "new" and can’t be initialized several times (probably). UPDATE: see: https://github.com/tbranyen/backbone.layoutmanager/wiki/Configuration –"Useful for when you want options to exist for all instances created on a specific View. Backbone.Layout.extend({ // Put options here..."
 	el:"#app",
-	template:_.template('<div id="imageList"></div><div id="imageMainView"></div>'),
+	template:_.template('<div id="imageList"></div><div id="MainView"></div>'),
 	initialize:function(){
 		this.listenTo(app, "imageListSelection", this.displayMockupView); //so the app’s main view can change if another image is selected to be displayed
 	},
@@ -16,6 +16,6 @@ app.AppView = Backbone.View.extend({
 		app.imageListView = this.setView("#imageList",new app.ImageListView({model:app.project}));//List displays all Mockups
 	},
 	displayMockupView:function(model){
-		this.setView("#imageMainView", new app.ImageMainView({model:model})).render() //TODO: called by whom?
+		this.setView("#MainView", new app.ImageMainView({model:model})).render() //TODO: called by whom?
 	}
 });
