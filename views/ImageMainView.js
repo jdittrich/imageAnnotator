d84c59a15422f4b68972061ca3f9706ed430f811
dittrich, jan <d_jan@ymail.com>
2015-08-16 20:31:40 +0200

    behaviour of navigation and manipulation of the image.

--- views/ImageMainView.js
+++ views/ImageMainView.js
@@ -1,4 +1,4 @@
-//renders 
+//renders
 //the current main image
 //sorta the viewport of the app
 
@@ -9,38 +9,66 @@
 var app = app || {};
 
 app.ImageMainView = Backbone.View.extend({
-	model:null,//not a native property, but makes sense here. Will be set.
-	el:false, //makes the outer element of your template the el
-	template:_.template('<div class="currentImageView">hey! <%= id %></div>'),
-	serialize:function(){
-		  return this.model.attributes;
+	model: null, //not a native property, but makes sense here. Will be set.
+	el: false, //makes the outer element of your template the el
+	//template:_.template('<div id="mainViewport"><div class="currentImageView"><img draggable="false" src="<%= filename %>" /></div></div>'),
+	template: _.template('<div class="currentImageView"><img draggable="false" src="<%= filename %>" /></div>'),
+	serialize: function () {
+		return this.model.attributes;
 	},
 	events: {
-		'click button.removeMockup':'destroyModel',
-		'drawrectsdrawn':'addLinkArea',
+		'click button.removeMockup': 'destroyModel',
+		'drawrectsdrawn': 'addLinkArea',
 	},
-	initialize:function(){
+	initialize: function () {
 		this.listenTo(this.model, 'destroy', this.remove);
-		this.listenTo(this.model, 'add:linkSources', this.displayNewLinkArea);//new link areas will be added via the addLinkArea function
+		this.listenTo(this.model, 'add:linkSources', this.displayNewLinkArea); //new link areas will be added via the addLinkArea function
 	},
-	afterRender:function(){
-		console.log("jq drawrects initialized?",this.$el.drawrects)
+	afterRender: function () {
+		//UI SETUP
+
+
+		// BEHAVIOUR SETUP
 		this.$el.drawrects({
-			removeElement:true,
-			distance:5,
-			allowSelector:"."+this.$el.attr("class")
+			removeElement: true,
+			distance: 5,
+			allowSelector: "." + this.$el.attr("class")
+		});
+		// wired bug: image zooming after "hard reset reload (shift+strg+R) works only if
+		// 1) rendering is triggered the 2nd time
+		// 2) the browser is reloaded normally again
+		// so probably: a. The HTML is rendered. b. This code executes and caches the dimensions of the dom elements c. the image is loaded and changes the dimensions of the dom elements to zoom - the caches cordinates are useless now and produce the "wrong" behaviour.
+		//possible fix: set one dimension (h or w) of the image in css.
+
+
+		var $panzoom = this.$panzoom = this.$el.panzoom(); //for testing purposes: can be deactivated via: $(".currentImageView").panzoom("disable");
+
+		$panzoom.on('mousewheel.focal', function (e) { //sets up zoom-to-coursor
+			e.preventDefault();
+			var delta = e.delta || e.originalEvent.wheelDelta;
+			var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
+			console.log(e.clientX, e)
+			$panzoom.panzoom('zoom', zoomOut, {
+				increment: 0.1,
+				animate: false,
+				focal: e
+			});
 		});
 	},
-	beforeRender:function(){
+	beforeRender: function () {
 		var that = this;
-		that.model.get("linkSources").each(function(element,index,list) {
-			that.insertView("",new app.LinkAreaView({model:element}));
+		that.model.get("linkSources").each(function (element, index, list) {
+			that.insertView("", new app.LinkAreaView({
+				model: element
+			}));
 		}, that);
 	},
-	addLinkArea:function(event, model){
+	addLinkArea: function (event, model) {
 		this.model.get("linkSources").add(model);
 	},
-	displayNewLinkArea:function(newModel){
-		this.insertView("",new app.LinkAreaView({model:newModel})).render();
+	displayNewLinkArea: function (newModel) {
+		this.insertView("", new app.LinkAreaView({
+			model: newModel
+		})).render();
 	}
 });
--- views/LinkAreaView.js
+++ views/LinkAreaView.js
@@ -6,38 +6,83 @@ var app = app || {};
 app.LinkAreaView = Backbone.View.extend({
 	model:null,//not a native property, but makes sense here. Will be set.
 	el:false, //makes the outer element of your template the el
-	template:_.template('<div class="linkArea"><%=width%><button class="removeLinkArea">x</button></div>'),
-	serialize:function(){
+	template:_.template('<div class="linkArea"><%=width%><button class="removeLinkArea">x</button><section class="areaComment"> <%= comment %></section><button class="makeEditable">edit</button></div>'),
+	serialize:function(){ //makes model attributes availiable in template
 		  return this.model.attributes;
 	},
 	events: {
 		'click button.removeLinkArea': 'destroyModel',
-		//'click button.setTarget':'setupSetTarget' //this was meant for linking, now we may want 
-    //several links and/or just some text
+		//'click button.setTarget':'setupSetTarget' //this was meant for linking, now we may want
+	//several links and/or just some text
 	},
 	initialize:function(){
 		this.listenTo(this.model, 'destroy', this.remove);
+		this.listenTo(this.model, 'change',this.render); //try something like app.project.get("mockups").at(0).get("linkSources").at(0).set("comment","bla") to test
 	},
 	beforeRender:function(){},
 	afterRender:function(){
 		var that = this;
+
+		//set the position and dimensions using the model data
+		//---------------------------------------------------
+		//TODO Future: use % data in models
 		that.$el.css({
 			left: that.model.get("left") + "px",
 			top: that.model.get("top") + "px",
 			width: that.model.get("width") + "px",
 			height: that.model.get("height") + "px"
 		});
+
+		// INTERACTIONS
+		// ------------
+
+		//Init reziable
+
 		that.$el.resizable({
-				containment: "parent"
-			});
-		
-		if(that.model.get("width")<10||that.model.get("height")<10){
+			containment: "parent"
+		});
+
+
+		//Init draggable
+
+		that.$el.draggable({
+			start:function(event,ui){
+				ui.position.left = 0;
+				ui.position.top = 0;
+				console.log("dragstart")
+			},
+			drag: function(event, ui) {//scaling fix: http://stackoverflow.com/questions/10212683/jquery-drag-resize-with-css-transform-scale
+				var zoomScale = that.el.getBoundingClientRect().width / that.el.offsetWidth; // http://stackoverflow.com/questions/5603615/get-the-scale-value-of-an-element
+				var changeLeft = ui.position.left - ui.originalPosition.left; // find change in left
+				var newLeft = ui.originalPosition.left + (changeLeft / zoomScale); // adjust new left by our zoomScale
+
+				var changeTop = ui.position.top - ui.originalPosition.top; // find change in top
+				var newTop = ui.originalPosition.top + (changeTop / zoomScale); // adjust new top by our zoomScale
+
+				ui.position.left = newLeft;
+				ui.position.top = newTop;
+			}
+		});
+
+		//Init edit comment function
+		that.$el.find(".areaComment").editable("dblclick", function(e){
+				that.changeComment(e.value);
+		});
+
+		// USER ERROR PREVENTION
+		//----------------------
+
+		////deletes if the area is very small and probably was only created by accident
+		if(that.model.get("width")<10||that.model.get("height")<10){ //deletes if the area is very small and probably was only created by accident
 			that.destroyModel();
+			return; //TODO programming debt: multiple exits: bad
 		}
+	},
+	changeComment:function(newValue){
+		this.model.set({comment:newValue});
 	},
 	destroyModel:function(event){
 		if(event){event.stopPropagation();}
-		
 		this.model.trigger("destroy",this.model, this.model.collection,{});
 	},
 	/*setupSetTarget:function(event){
