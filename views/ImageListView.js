b7f26ea7abae2b9fdc0b8923515e540dc64aa0fc
dittrich, jan <d_jan@ymail.com>
2015-08-16 20:32:23 +0200

    setup list with interaction, comments and file name retrieval

--- views/ImageListView.js
+++ views/ImageListView.js
@@ -10,12 +10,14 @@ var app = app || {};
 app.ImageListView = Backbone.View.extend({
 	model:null,//not a native property, but makes sense here. Will be set.
 	el:false, //makes the outer element of your template the el
-	template:_.template('<div><button class="newMockup">NewStuff!</button><ul id="mockupList"></ul></div>'),
+	template:_.template('<div><input class="newMockup" type="file"\>NewStuff!</input><ul id="mockupList"></ul></div>'),
 	events: {
-		'click button.newMockup': 'newMockupModel',
-
+		// 'click button.newMockup': 'newMockupModel',
+		'change input.newMockup':'newMockupModel'
 	},
 	initialize:function(){
+		//this needs to be adjusted according to the environment: Browser or nw.js/electron. Both use the input type=file element; The access to the image content is different though.
+		//for now we use just the name of the file and assume/demand that it is located aside (or, with some adjustments in the model, below) the index.html!
 		this.listenTo(this.model.get("mockups"),"add",this.displayNewListItem)
 	},
 	beforeRender:function(view){
@@ -33,8 +35,10 @@ app.ImageListView = Backbone.View.extend({
 		// this.renderViews();
 		newView.render(); //try this
 	},
-	newMockupModel:function(){
-		this.model.get("mockups").add({title:"someTitel"});
+	newMockupModel:function(event){
+		var filename = $(event.target).val().replace(/.*(\/|\\)/, '');
+		if(filename.length>0){ //prevent against empty values
+			this.model.get("mockups").add({filename:filename});
+		}
 	}
 });
-	
